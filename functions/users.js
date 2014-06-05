5d6ee0ec61bfe3fd91974231fa173a725e7c5d48
bcokca <bcokca@gmail.com>
2014-06-05 00:26:30 -0700

    functions added

---
+++ functions/users.js
@@ -0,0 +1,26 @@
+// COMMON INCLUDES
+//var db = require("./../includes/database");
+//var cache = require('./../includes/cache');
+//var utilities = require('./../includes/utilities');
+//var val= require('./../validation');
+//var async = require('async');
+//var SessionManager = require('./../includes/session');
+//var Hapi = require('hapi');
+
+// initiate params
+//var queryParameters = '';
+//var sqlEndingClause = '';
+
+
+//***************************************************************************************************************************
+// allUsersInformation
+exports.healthCheck = function (request) {
+
+    request.reply (JSON.stringify('Hello World'));
+
+};
+
+
+
+
+
