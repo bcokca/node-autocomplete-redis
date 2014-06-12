14b7c736d39a5fd4b7a821e635d5cebfb1258f12
bcokca <bcokca@gmail.com>
2014-06-12 02:16:04 -0700

    example usage for autocomplete

--- example-usage/lookup.js
+++ example-usage/lookup.js
@@ -1,22 +1,66 @@
 var search = require('../lib/search');
 
+//var cache = require('../lib/cache');
+
 //***************************************************************************************************************************
 // allUsersInformation
 exports.getSuggestions = function (request) {
 
     var value = request.params.value;
     var key = request.params.key;
-      var arr = ['dwa', 'dwad'];
-   // search.cacheData('myKey', ['mahmut', 'sevki']);
+    //var arr = ['dwa', 'dwad'];
 
-    //search.getTheValue(key, value, function(err, arr){
+    var myArray = ['burhan', 'mahmut'];
 
-        request.reply (JSON.stringify(arr));
+    search.cacheData(key, myArray,function(error){
+        if(error){
+            console.log('error occured');
 
-    //});
+            search.getTheValue(key, value,function(err, result){
+
+                if(err){
+                    console.log(err);
+                    request.reply(err);
+
+                }else{
+                    console.log(result);
+                    request.reply(result);
+                }
+
+            });
 
 
+        };
 
+
+    });
+
+              /*
+    //check if key is exist
+    cache.getZrange(key, value,  function(err,obj){
+
+        if(err){request.reply(err);return false;}
+
+        //if cache is not empty
+        if(obj){
+            //if key exist return the mapped values
+            cache.hashMultipleGet(key, obj, function(err2, obj2){
+                if(err2){request.reply(err2); return false;}
+                console.log(obj2);
+                request.reply(JSON.stringify(obj2));
+                return true;
+            });
+        }else{
+
+
+               //else we need to cache it
+              search.cacheData(key, myArray);
+            request.reply(JSON.stringify(myArray));
+
+        }
+    });
+
+            */
 
 
 };
